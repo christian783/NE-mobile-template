6ad290d109b33b2c76be06e01f31ccccd629b197
christian783 <christiantc0000@gmail.com>
2026-05-28 08:22:20 +0200

    initial commit

--- app/constants/config.js
+++ app/constants/config.js
@@ -7,7 +7,7 @@ export const SERVER_BASE_URL =
 
 export const MOCK_API_BASE_URL =
   process.env.EXPO_PUBLIC_MOCK_API_BASE_URL ||
-  'https://6a17d7511878294b597beb61.mockapi.io/api/v1';
+  'https://your-project-id.mockapi.io/api/v1';
 
 export const USE_MOCK_API = API_PROVIDER === 'mockapi';
 export const BASE_URL = USE_MOCK_API ? MOCK_API_BASE_URL : SERVER_BASE_URL;
